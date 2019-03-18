d7d3426b718726b753a0b622f4bf33a54e52ef4d
JIZHAOPING <670237342@qq.com>
2019-03-18 21:43:13 +0800

    yml

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,14 +17,27 @@ module.exports = function(grunt){
       options: {
         htmlhintrc: '.htmlhintrc'          
       },
-      src: '*.html'               
-    }    
+      src: '*.html'
+    },
+    mocha: {
+      test: {
+          src: ['test/index.html'],                    
+      },
+      options: {
+          run: true,
+          reporter: 'Spec'                              
+      }
+          
+    }     
+      
   });
 
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha');
 
   grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+  grunt.registerTask('unitTest', ['mocha']);
 };
 
